ad75254d75b30812399baca41008aedbd456e32c
沐姜子 <@DESKTOP-B5M3HC1>
2021-03-09 09:15:11 +0800

    Revert "添加3D Points Cloud系统页面"
    This reverts commit f818297c6da3349bd043633d370029b8bd01744f.

--- MVC/Utility/UserManage.cs
+++ MVC/Utility/UserManage.cs
@@ -163,10 +163,6 @@ namespace MVC
                     {
                         return MODEL.Enum.SystemRole.Uav;
                     }
-                    else if (role.RoleName.ToUpper() == MODEL.Enum.SystemRole.pointsCloud.ToString().ToUpper())
-                    {
-                        return MODEL.Enum.SystemRole.pointsCloud;
-                    }
                     else
                     {
                         return MODEL.Enum.SystemRole.Null;
