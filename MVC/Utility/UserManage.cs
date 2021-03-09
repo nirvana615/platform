dfa3efe8836a033c0e521ee6028ca3682de875e3
沐姜子 <@DESKTOP-B5M3HC1>
2021-03-09 15:09:24 +0800

    test

--- MVC/Utility/UserManage.cs
+++ MVC/Utility/UserManage.cs
@@ -163,6 +163,10 @@ namespace MVC
                     {
                         return MODEL.Enum.SystemRole.Uav;
                     }
+                    else if (role.RoleName.ToUpper() == MODEL.Enum.SystemRole.pointsCloud.ToString().ToUpper())
+                    {
+                        return MODEL.Enum.SystemRole.pointsCloud;
+                    }
                     else
                     {
                         return MODEL.Enum.SystemRole.Null;
