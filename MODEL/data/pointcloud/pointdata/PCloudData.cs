c97908edad980070d76a1a36ef7590f3dbe56708
沐姜子 <@DESKTOP-B5M3HC1>
2021-04-20 11:52:12 +0800

    添加点云MODEL

--- MODEL/data/pointcloud/pointdata/PCloudData.cs
+++ MODEL/data/pointcloud/pointdata/PCloudData.cs
@@ -15,11 +15,11 @@ namespace MODEL
         /// <summary>
         /// id
         /// </summary>
-        public int ProjectId { get; set; }
+        public int  ProjectId { get; set; }
         /// <summary>
-        /// 点云路径
+        /// 项目名称
         /// </summary>
-        public string MXLJ { get; set; }
+        public string XMMC { get; set; }
         /// <summary>
         /// 点云采集时间
         /// </summary>
@@ -27,7 +27,7 @@ namespace MODEL
         /// <summary>
         /// SRID
         /// </summary>
-        public int? SRID { get; set; }
+        public string SRID { get; set; }
         /// <summary>
         /// 标识码
         /// </summary>
@@ -44,7 +44,57 @@ namespace MODEL
         /// <summary>
         /// 区域
         /// </summary>
-        public int QY { get; set; }
+        public int Regionid { get; set; }
+
+        /// <summary>
+        /// 采集人员
+        /// </summary>
+        public string CJRY { get; set; }
+
+        /// <summary>
+        /// 数据格式
+        /// </summary>
+        public int SJGSid { get; set; }
+
+        /// <summary>
+        /// 采集设备
+        /// </summary>
+        public int Deviceid { get; set; }
+
+        /// <summary>
+        /// 数据类型
+        /// </summary>
+        public int Typeid { get; set; }
+
+        /// <summary>
+        /// 点云数目
+        /// </summary>
+        public string DYSM { get; set; }
+
+        /// <summary>
+        /// 目前流程
+        /// </summary>
+        public int MQLCid { get; set; }
+
+
+        /// <summary>
+        /// 采集周期
+        /// </summary>
+        public int CJZQ { get; set; }
+
+        /// <summary>
+        /// 中心经度
+        /// </summary>
+        public string  ZXJD { get; set; }
+        /// <summary>
+        /// 中心纬度
+        /// </summary>
+        public string ZXWD { get; set; }
+
+        /// <summary>
+        /// 项目工程设置
+        /// </summary>
+        public ProjectSetUp projectSet { get; set; }
 
     }
 }
